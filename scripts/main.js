6eb1680e6db4ca00bd4922aedc2f48549dc88619
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-10-11 12:37:50 -0600

    Fixed code with help from caroline by finishing loop and adding return method

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,17 @@ let bandNumber = 1
 
 // I know I need to Write an Impure Function here but I am too stupid to do that...
 
-const takeNumber = function (argumentsgohere) {
-  // for (bandNumber = 0; bandNumber < bandNumber.length; bandNumber++)
-  
-  //   if (bandNumber > 1) {
-  //      console.log(`1. ${bandNumber}`)
-  //  }
+const takeNumber = function (bandName) {
+  for (bandNumber.length; bandNumber++;)
+    return (`${bandNumber-1}.  ${bandName}`)
    
-
+}
 // These const are to be used as arguments in the function
 const scum = takeNumber("Galactic Scum") 
 console.log(scum)  // This should print "1. Galactic Scum" in the console
 
 const under = takeNumber("Underdogs") 
 console.log(under)  // This should print "2. Underdogs" in the console
-}
+
+const thePrettyReckless = takeNumber("The Pretty Reckless")
+console.log(thePrettyReckless)
